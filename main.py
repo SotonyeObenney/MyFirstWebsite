d690643dfe37fb88af9e0346bd691789b4ea980e
SotonyeObenney <obenneysotonye@gmail.com>
2025-10-16 19:41:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-from dotenv import load_dotenv
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
